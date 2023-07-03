01ad26c11a5192da05addae6302c1ea6fc558b16
Hakan Güldal <hguldal@gmail.com>
2023-07-03 23:02:19 +0300

    Some improvements

--- src/EduStatTests.py
+++ src/EduStatTests.py
@@ -9,47 +9,58 @@ class EduStatTests:
 
   def LoadFromCSV(self,CSVPath):
     self.data = pd.read_csv(CSVPath)
- 
+
   def LoadFromExcel(self,ExcelPath):
     self.data = pd.read_csv(ExcelPath)
 
-  def IndependedTTest(self,variable1,variable2):
+  def IndTTest(self,variable1,variable2):
     dataCollection=dict()
-   
+
     if self.data is None:
       raise Exception("No data loaded")
 
     if len(self.data[variable1].value_counts().index)>2:
       raise Exception("The number of groups cannot be greater than 2")
 
-    for idx in self.data[variable1].value_counts().index:  
+    for idx in self.data[variable1].value_counts().index:
       dataCollection[idx]={"data":self.data.query(variable1 + "==" + str(idx))[variable2]}
-    
+
     rawData=list()
     for item in dataCollection:
       rawData.append(dataCollection[item]["data"])
 
     data1,data2=rawData[0],rawData[1]
     result1 = ttest_ind(data1, data2)
-    result2=levene(data1, data2)
+    result2=levene(data1, data2,center='mean')
     result3= ttest_ind(data1, data2,equal_var = False)
     dofF=len(data1)+len(data2)-2
-
+    groupStats=list()
     for item in dataCollection:
-      dataCollection[item]["N"]=dataCollection[item]["data"].count()
-      dataCollection[item]["Mean"]=dataCollection[item]["data"].mean()
-      dataCollection[item]["StdDev"]=dataCollection[item]["data"].std()
+      group=dict()
+      group[item]={"N":dataCollection[item]["data"].count(),
+                   "Mean":dataCollection[item]["data"].mean(),
+                   "StdDev":dataCollection[item]["data"].std()
+      }
+      groupStats.append(group)
     
     dictReturn={
-        "groupStats":{"N":self.data[variable1].value_counts().to_dict()},
-        
-        "equalVariancesAssumed":{
-            "ttestForEqualityOfMeans": {
-                "t":result1[0],
-                "df":dofF,
-                "sigTwoTailed":result1[1]
-            }
+        "groupStats":groupStats,
 
+        "TTest":{
+            "t":result1[0],
+            "df":dofF,
+            "sigTwoTailed":result1[1]
+        },
+        
+        "LeveneTest":
+        {
+         "F":result2[0],
+         "sigTwoTailed":result2[1]
+            
+        },
+        "WelchTest":{
+            "t":result3[0],
+            "sigTwoTailed":result3[1]
         }
     }
 
