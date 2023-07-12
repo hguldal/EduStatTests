a17e9971966554f2a47db308eb52a03f2e47ce1a
Hakan Güldal <hguldal@gmail.com>
2023-07-12 10:49:18 +0300

    Read the Docs confs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'EduStatTests'
+copyright = '2023'
+author = 'Hakan Güldal'
 
 release = '0.1'
 version = '0.1.0'
